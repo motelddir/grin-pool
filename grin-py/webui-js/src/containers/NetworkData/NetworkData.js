be052aea799b70613d140d4234cfc1c23709ace7
motelddir <47373416+motelddir@users.noreply.github.com>
2019-02-07 02:47:16 +0100

    Add files via upload

--- grin-py/webui-js/src/containers/NetworkData/NetworkData.js
+++ grin-py/webui-js/src/containers/NetworkData/NetworkData.js
@@ -54,7 +54,7 @@ export class NetworkDataComponent extends Component {
     return (
       <Row xs={12} md={12} lg={12} xl={12}>
         <Col xs={12} md={12} lg={5} xl={3}>
-          <h4 className='page-title' style={{ marginBottom: 36 }}>Network Stats</h4>
+          <h4 className='page-title' style={{ marginBottom: 36 }}>BitGrin Network Stats</h4>
           <Table size='sm'>
             <tbody>
               <tr>
@@ -75,7 +75,7 @@ export class NetworkDataComponent extends Component {
               </tr>
               <tr>
                 <td><FontAwesomeIcon style={{ marginRight: 5 }} size='lg' icon={'dollar-sign'} />Reward</td>
-                <td>60 GRIN / block</td>
+                <td>4.5 XBG / block</td>
               </tr>
             </tbody>
           </Table>
