dac8031a2b56bb521ec2fc2844ac1a4311b20706
motelddir <47373416+motelddir@users.noreply.github.com>
2019-02-07 03:18:48 +0100

    Add files via upload

--- grin-py/webui-js/src/containers/Instructions/Instructions.js
+++ grin-py/webui-js/src/containers/Instructions/Instructions.js
@@ -20,19 +20,13 @@ export class InstructionsComponent extends Component {
         <Card>
           <CardBody>
             <Row>
-              <Col xs={12} md={6} lg={6} xl={6}>
+              <Col xs={12} md={9} lg={9} xl={9}>
                 <h2>How to Mine BitGrin Tutorials</h2>
                 <p style={{ fontSize: '1.1rem' }}>For those of you who prefer written instructions, please check out our written tutorial <a href='https://medium.com/@blade.doyle/gpu-mining-on-mwgrinpool-com-how-to-72970e550a27' rel='noopener noreferrer' target='_blank'>here</a>.</p>
-                <p style={{ fontSize: '1.1rem' }}>If you prefer following a video tutorial, we got you covered! Check out our video tutorial <a href='https://youtu.be/eRtzwo6W8iE'>here</a>.</p>
               </Col>
               <Col xs={12} md={3} lg={3} xl={3}>
                 <div className='article-tutorial-thumbnail'>
                   <a href='https://medium.com/@blade.doyle/gpu-mining-on-mwgrinpool-com-how-to-72970e550a27'><img src='/img/gpu-medium-article-overlayed.png' /></a>
-                </div>
-              </Col>
-              <Col xs={12} md={3} lg={3} xl={3}>
-                <div className='video-tutorial-thumbnail'>
-                  <a href='https://youtu.be/eRtzwo6W8iE'><img src='/img/gpu-video-tutorial-overlayed.png' /></a>
                 </div>
               </Col>
             </Row>
