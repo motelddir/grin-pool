04ddaec760eb5c3109ba1a15d0b1bb2eb20e8d66
motelddir <47373416+motelddir@users.noreply.github.com>
2019-02-07 02:38:44 +0100

    Add files via upload

--- grin-py/webui-js/src/containers/About/About.js
+++ grin-py/webui-js/src/containers/About/About.js
@@ -8,18 +8,18 @@ export class AboutComponent extends Component {
         <Card>
           <CardBody>
             <div className='card__title'>
-              <h5 className='bold-text'>Open-Source Mining Pool for the MimbleWimble Grin Blockchain</h5>
+              <h5 className='bold-text'>Open-Source Mining Pool for the MimbleWimble BitGrin Blockchain</h5>
             </div>
-            <h4 className='bold-text'>About MWGrinPool</h4>
-            <p style={{ fontWeight: 'bold' }}>GRIN is currently mining on FlooNet, with MainNet expected to launch January 15th 2019</p>
-            <Alert color='danger' style={{ width: '50%', textAlign: 'center' }}>Please be warned that FlooNet GRIN coins have no significant value.</Alert>
+            <h4 className='bold-text'>About BitGrinPool</h4>
+            <p style={{ fontWeight: 'bold' }}>BitGrin is currently mining on TestNet, with MainNet expected to launch Febuary 9th 2019</p>
+            <Alert color='danger' style={{ width: '50%', textAlign: 'center' }}>Please be warned that TestNet BitGrin coins have no significant value.</Alert>
             <h4>How to mine in this pool:</h4>
             <ul>
-              <li>Supports Linux and Windows miners: mimblewimble/grin-miner and mozkomor/GrinGoldMiner</li>
+              <li>Supports Linux and Windows miners: mimblewimble/bitgrin/grin-miner and mozkomor/GrinGoldMiner</li>
               <li><a href="https://medium.com/@blade.doyle/cpu-mining-on-mwgrinpool-com-how-to-efb9ed102bc9">CPU Mining Guide</a></li>
               <li><a href="https://medium.com/@blade.doyle/gpu-mining-on-mwgrinpool-com-how-to-72970e550a27">GPU Mining Guide</a></li>
               <li><a href="https://medium.com/@blade.doyle/configure-payments-on-mwgrinpool-com-how-to-7b84163ec467">Payment Configuration Guide</a></li>
-              <li><a href="https://gitter.im/grin-pool/Lobby">Gtter Discussion Channel</a></li>
+              <li><a href="https://t.me/BitGrinCommunity">BitGrin Telegram Group</a></li>
             </ul>
           </CardBody>
         </Card>
